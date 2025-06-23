4271f59710cf2c8278bfaceec6e5f6614cad781b
SurgeOwner <owner.surge@gmail.com>
2025-06-22 22:41:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: '/surgeofficial.github.io/', // Replace 'your-repo-name' with your actual GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
